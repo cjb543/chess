30fc98426d887038d2dce29d4e7738872cb8ff12
cjb543 <cjb543@protonmail.com>
2025-03-09 23:56:16 -0700

    Initialize all needed buttons

---
+++ pgn-visualizer/board.py
@@ -0,0 +1,32 @@
+from PyQt6.QtWidgets import QWidget
+from PyQt6.QtGui import QPainter, QColor
+from PyQt6.QtCore import Qt
+
+class ChessBoard(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.square_size = 40
+        self.board_size = self.square_size * 8
+        self.setMinimumSize(256,256)
+    
+    def resizeEvent(self, event):
+        available_space = min(self.width(),self.height())
+        self.board_size = available_space - 20
+        self.square_size = self.board_size // 8
+        self.board_size = self.square_size * 8
+        super().resizeEvent(event)
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.RenderHint.Antialiasing)
+        start_x = (self.width() - self.board_size) // 2
+        start_y = (self.height() - self.board_size) // 2
+        for row in range(8):
+            for col in range(8):
+                x = start_x + col * self.square_size
+                y = start_y + row * self.square_size
+                if (row + col) % 2 == 0:
+                    color = QColor(240,217,181)
+                else:
+                    color = QColor(181,136,99)
+                painter.fillRect(x, y, self.square_size, self.square_size, color)
--- pgn-visualizer/main.py
+++ pgn-visualizer/main.py
@@ -1,13 +1,40 @@
+from PyQt6.QtCore import QSize, Qt
 from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow
 import sys
+from board import ChessBoard
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        # Set Program Title
         self.setWindowTitle("PGN Visualizer")
-        button = QPushButton("Hello World")
-        self.setCentralWidget(button)
+
+        # Set Program Size to 640x480 fixed
+        self.setFixedSize(QSize(640,480))
+
+        # Initialize and show Chess Board
+        self.board = ChessBoard()
+        self.setCentralWidget(self.board)
+
+        # Initialize Upload Button
+        upload_button = QPushButton("Upload")
+
+        # Initialize Next Move Arrow
+        next_move_button = QPushButton("Next Move")
+        next_move_button.isCheckable = True
+        next_move_button.clicked.connect(self.nextMove)
+
+        # Initialize Previous Move Arrow
+        previous_move_button = QPushButton("Previous Move")
+        previous_move_button.isCheckable = True
+        previous_move_button.clicked.connect(self.previousMove)
+
+    def nextMove(self):
+        print("Next Move Clicked!")
+
+    def previousMove(self):
+        print("Previous Move Clicked!")
 
 app = QApplication(sys.argv)
 window = MainWindow()
